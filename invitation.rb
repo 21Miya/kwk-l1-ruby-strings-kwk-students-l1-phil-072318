e05cde9ece9acf7a0de04e9c0f13ad23d41b9930
21Miya <mmshin@live.com>
2018-07-24 14:34:18 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,7 +1,7 @@
 # Code your prompts here!
 
 # Try starting out with puts'ing a string.
-puts "Who is being invited to the party?"
+puts "Hi, you've been invited to a party! What is your name?"
 guest_name = gets.chomp.capitalize
 
 puts "What's the name of the party?"
